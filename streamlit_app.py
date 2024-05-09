ac37d121b68b50881be67e41c400031ca9b5f707
Sam Chung <tieoneease@gmail.com>
2024-05-09 14:31:13 +0000

    load from env

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,29 @@ import os
 import streamlit as st
 from google.cloud import bigquery
 import pandas as pd
+import os
+import base64
+import json
+from google.oauth2 import service_account
+
+# Retrieve the encoded JSON from the environment variable
+encoded_json = os.getenv('GOOGLE_CREDENTIALS')
+
+# Decode the JSON back to its original format
+json_creds = base64.b64decode(encoded_json).decode()
+
+# Convert the JSON string back to a dictionary
+credentials_dict = json.loads(json_creds)
+
+# Create credentials from the service account dictionary
+credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+
 import nps
 import referrals
 
-# Set the Google Cloud credentials environment variable
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service-account.json"
-
 # Initialize BigQuery client
 project = 'peachy-268419'
-client = bigquery.Client(project=project)
+client = bigquery.Client(project=project, credentials=credentials)
 
 # Streamlit interface
 start_date = st.sidebar.date_input("Start Date", value=pd.to_datetime("2023-01-01"))
